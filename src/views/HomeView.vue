0d3880503dc16cf5a509a5ce0c9c43baa4a80f9c
daniel <online.danielakinschin@gmail.com>
2024-12-14 06:28:18 +0100

    misc: cleanup

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,9 +1,7 @@
-<script setup>
-import TheWelcome from '../components/TheWelcome.vue'
-</script>
+<script setup></script>
 
 <template>
-  <main>
-    <TheWelcome />
-  </main>
+  <h1>Hello World</h1>
 </template>
+
+<style scoped></style>
